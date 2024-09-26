e865f2649859b618ceee238609d942d5a9a7f64f
Emmanuel Patricio <L192909399@cdguzman.tecnm.mx>
2024-09-26 10:33:53 -0600

    Deploy Next.js static site to GitHub Pages

---
+++ text/brand.js
@@ -0,0 +1,14 @@
+module.exports = {
+  saas: {
+    name: 'FoodHub',
+    desc: 'FoodHub - Build a restaurant that works for you',
+    prefix: 'FoodHub',
+    footerText: 'FoodHub 2024',
+    logoText: 'FoodHub',
+    projectName: 'FoodHub',
+    developed: 'Developed by Jimbo',
+    url: 'luxireact.ux-maestro.com/saas',
+    img: '/static/images/saas-logo.png',
+    notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+};
---
+++ text/link.js
@@ -0,0 +1,10 @@
+const link = {
+  saas: {
+    home: '/',
+    contact: '/contact',
+    login: '/login',
+    register: '/contact'
+  }
+};
+
+export default link;
